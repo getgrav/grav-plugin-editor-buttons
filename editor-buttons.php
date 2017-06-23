278aacf170beb1246d96b72359fd0e6993dc0ce8
dennemark <dennemark@posteo.de>
2017-06-23 16:04:56 +0200

    Update editor-buttons.php

--- editor-buttons.php
+++ editor-buttons.php
@@ -43,30 +43,14 @@ class EditorButtonsPlugin extends Plugin
               $this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-notice/css/notice.css');
             }
 
-            $shortcodesCORE = $this->config->get('plugins.editor-buttons.insert_shortcodes.alignleft')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.aligncenter')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.alignright')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.column')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.safeemail')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.notice')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.underline')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.size');
-
-            if ($this->config->get('plugins.shortcode-core.enabled') && $this->config->get('plugins.editor-buttons.insert_shortcodes') || $shortcodesCORE) {
+            if ($this->config->get('plugins.shortcode-core.enabled') && $this->config->get('plugins.editor-buttons.insert_shortcodes')) {
               $this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/js/shortcodes-core.js');
-              $this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/css/shortcodes-core.css');
+              //$this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/css/shortcodes-core.css');
             }
 
-            $shortcodesUI = $this->config->get('plugins.editor-buttons.insert_shortcodes.tab')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.accordion')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.callout')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.browser')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.polaroid')
-                || $this->config->get('plugins.editor-buttons.insert_shortcodes.imagecompare');
-
-            if ($this->config->get('plugins.shortcode-ui.enabled') && $this->config->get('plugins.editor-buttons.insert_shortcodes') || $shortcodesUI) {
+            if ($this->config->get('plugins.shortcode-ui.enabled') && $this->config->get('plugins.editor-buttons.insert_shortcodes')) {
               $this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/js/shortcodes-ui.js');
-              $this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/css/shortcodes-ui.css');
+              //$this->grav['assets']->add('plugin://editor-buttons/admin/buttons/insert-shortcodes/css/shortcodes-ui.css');
             }
 
         }
